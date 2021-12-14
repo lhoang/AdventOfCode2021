aa35d87b9ffb7e89224a15c9596236b5bdb34012
Lang Hoang <lang.hoang@radiofrance.com>
2021-12-14 21:28:27 +0100

    Day 14 ⭐️⭐️ - Part 2 🤯

--- src/day14/ExtendedPolymerization.spec.ts
+++ src/day14/ExtendedPolymerization.spec.ts
@@ -2,8 +2,14 @@ import { readFile } from '../utils/input'
 import {
   applyStepN,
   countAfterNStep,
+  countAfterNStepRepartition,
+  diffMaxMin,
+  optimizedStep,
   parse,
+  parseRepartition,
+  repartitionAfterNStep,
   step,
+  stepRepartition,
 } from './ExtendedPolymerization'
 
 const realInput = readFile('day14/input.txt')
@@ -46,10 +52,33 @@ describe('Extended Polymerization', () => {
     )
   })
 
+  it('should apply step (Optimized)', () => {
+    const applyStep = optimizedStep(rules)
+    const start1 = [...'NNCB']
+    applyStep(start1)
+    expect(start1.join('')).toEqual('NCNBCHB')
+    const start2 = [...'NBBBCNCCNBBNBNBBCHBHHBCHB']
+    applyStep(start2)
+    expect(start2.join('')).toEqual(
+      'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB',
+    )
+  })
+
   it('should apply step n times', () => {
     expect(applyStepN(rules, 'NNCB', 4)).toEqual(
       'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB',
     )
+  })
+
+  it('should get repartition after n steps', () => {
+    const rep3 = repartitionAfterNStep(rules, 'NNCB', 3)
+    expect(rep3).toEqual({ N: 5, B: 11, C: 5, H: 4 })
+
+    const rep4 = repartitionAfterNStep(rules, 'NNCB', 4)
+    expect(rep4).toEqual({ N: 11, B: 23, C: 10, H: 5 })
+
+    const rep10 = repartitionAfterNStep(rules, 'NNCB', 10)
+    expect(rep10).toEqual({ N: 865, B: 1749, C: 298, H: 161 })
   })
 
   it('should count diff max min occurrences after 10 steps ⭐️', () => {
@@ -57,7 +86,92 @@ describe('Extended Polymerization', () => {
     expect(countAfterNStep(realInput, 10)).toEqual(3247)
   })
 
-  it('should count diff max min occurrences after 40 steps ⭐⭐️️', () => {
-    //expect(countAfterNStep(input, 40)).toEqual(2_188_189_693_529)
+  it('should parse repartition', () => {
+    const [repartition, rules] = parseRepartition(input)
+    expect(rules.size).toEqual(16)
+    expect(rules.get('HH')).toEqual(['HN', 'NH'])
+    expect(rules.get('NC')).toEqual(['NB', 'BC'])
+    expect(repartition).toEqual(
+      expect.objectContaining({
+        NN: 1,
+        NC: 1,
+        CB: 1,
+        BC: 0,
+      }),
+    )
+  })
+
+  it('should apply step for repartition', () => {
+    const [repartition, rules] = parseRepartition(input)
+    // NNCB
+    const rep1 = stepRepartition(rules, repartition)
+    // NCNBCHB
+    // console.log({ rep1 })
+    expect(rep1).toEqual(
+      expect.objectContaining({
+        NC: 1,
+        CN: 1,
+        NB: 1,
+        BC: 1,
+        CH: 1,
+        HB: 1,
+        CB: 0,
+      }),
+    )
+    const rep2 = stepRepartition(rules, rep1)
+    // NBCCNBBBCBHCB
+    expect(rep2).toEqual(
+      expect.objectContaining({
+        CB: 2,
+        BC: 2,
+        BH: 1,
+      }),
+    )
+    const rep3 = stepRepartition(rules, rep2)
+    // NBBBCNCCNBBNBNBBCHBHHBCHB
+    // { N: 5, B: 11, C: 5, H: 4 }
+    expect(rep3).toEqual(
+      expect.objectContaining({
+        BB: 4,
+        BC: 3,
+        BH: 1,
+      }),
+    )
+  })
+
+  it('should count diff max min from repartition', () => {
+    // NBBBCNCCNBBNBNBBCHBHHBCHB
+    // { N: 5, B: 11, C: 5, H: 4 }
+    const repartition = {
+      CB: 0,
+      BH: 1,
+      HC: 0,
+      HH: 1,
+      HN: 0,
+      NH: 0,
+      HB: 3,
+      BC: 3,
+      CC: 1,
+      CN: 2,
+      NB: 4,
+      CH: 2,
+      BB: 4,
+      BN: 2,
+      NC: 1,
+    }
+
+    expect(diffMaxMin(repartition)).toEqual(7)
+  })
+
+  it('should count diff max min occurrences after 10 steps ⭐ (repartition)️', () => {
+    expect(countAfterNStepRepartition(input, 10)).toEqual(1588)
+    expect(countAfterNStepRepartition(realInput, 10)).toEqual(3247)
+  })
+
+  it('should count diff max min occurrences after 40 steps ⭐⭐ (repartition)️', () => {
+    expect(countAfterNStepRepartition(input, 40)).toEqual(2_188_189_693_529)
+
+    const superRes = countAfterNStepRepartition(realInput, 40)
+    expect(superRes).toEqual(4_110_568_157_153)
   })
 })
--- src/day14/ExtendedPolymerization.ts
+++ src/day14/ExtendedPolymerization.ts
@@ -1,5 +1,6 @@
 import { range, split } from '../utils/input'
-import { countBy, identity } from 'ramda'
+import { add, countBy, identity, mergeWith } from 'ramda'
+import { performance } from 'perf_hooks'
 
 export function parse(input: string): [string, Map<string, string>] {
   const [start, rulesPart] = input.split(`\n\n`)
@@ -14,6 +15,12 @@ export function parse(input: string): [string, Map<string, string>] {
   return [start, rules]
 }
 
+/**
+ * Naive approach : work on the string
+ * Works only for part 1.
+ * Step : 17; Acc size : 196609; Time: 0.03435437500476837 s
+ * Step : 24; Acc size : 25165825; Time: 6.9526850829720495 ms
+ */
 export const step =
   (rules: Map<string, string>) =>
   (s: string): string => {
@@ -31,7 +38,84 @@ export function applyStepN(
   n: number,
 ): string {
   const applyStep = step(rules)
-  return range(1, n).reduce((acc, _) => applyStep(acc), start)
+  return range(1, n).reduce((acc, i) => {
+    const startTime = performance.now()
+    const res = applyStep(acc)
+    const endTime = performance.now()
+    console.log(
+      `Step : ${i}; Acc size : ${acc.length}; Time: ${
+        (endTime - startTime) / 1000
+      } s`,
+    )
+    return res
+  }, start)
+}
+
+/**
+ * Use of splice
+ * Not optimized...
+ * Step : 17; Acc size : 393217; Time: 11.481643917024135 s
+ */
+export const optimizedStep = (rules: Map<string, string>) => (s: string[]) => {
+  for (let i = 0; i < s.length - 1; i = i + 2) {
+    const insert = rules.get(s[i] + s[i + 1])
+    s.splice(i + 1, 0, insert)
+  }
+}
+
+export function applyOptimizedStepN(
+  rules: Map<string, string>,
+  start: string,
+  n: number,
+): string {
+  const applyStep = optimizedStep(rules)
+  const acc = [...start]
+  range(1, n).forEach(i => {
+    const startTime = performance.now()
+    applyStep(acc)
+    const endTime = performance.now()
+    console.log(
+      `Step : ${i}; Acc size : ${acc.length}; Time: ${
+        (endTime - startTime) / 1000
+      } s`,
+    )
+  })
+  return acc.join('')
+}
+
+/**
+ * Split / Map / Reduce
+ * Not optimized...
+ * Step 17 : Time: 0.13946350002288818 s
+ */
+export function repartitionAfterNStep(
+  rules: Map<string, string>,
+  start: string,
+  n: number,
+) {
+  const limit = 1000
+
+  const applyStep = step(rules)
+
+  const recRep = (steps: number, str: string): Record<string, number> => {
+    if (steps == 0) {
+      return countBy(identity, [...str])
+    }
+    const newStr = applyStep(str)
+    if (newStr.length < limit) {
+      return recRep(steps - 1, newStr)
+    } else {
+      const t1 = newStr.substr(0, Math.ceil(newStr.length / 2))
+      const splitIndex = Math.floor(newStr.length / 2)
+      const t2 = newStr.substr(splitIndex)
+      const s1 = recRep(steps - 1, t1)
+      const s2 = recRep(steps - 1, t2)
+      const sum = mergeWith(add, s1, s2)
+      sum[newStr[splitIndex]]--
+      return sum
+    }
+  }
+  return recRep(n, start)
 }
 
 export function countAfterNStep(input: string, n: number): number {
@@ -40,4 +124,78 @@ export function countAfterNStep(input: string, n: number): number {
   const countMap = countBy(identity, [...res])
   const values = Object.values(countMap)
   return Math.max(...values) - Math.min(...values)
+}
+
+/**
+ * Work on the pairs, not the string.
+ */
+export function parseRepartition(
+  input: string,
+): [Record<string, number>, Map<string, string[]>] {
+  const [start, rulesPart] = input.split(`\n\n`)
+
+  const rules = new Map<string, string[]>(
+    split(rulesPart).map(line => {
+      const [pair, insert] = line.split(' -> ')
+      return [pair, [pair[0] + insert, insert + pair[1]]]
+    }),
+  )
+
+  const repartition = Object.fromEntries([...rules.keys()].map(key => [key, 0]))
+
+  range(0, start.length - 2)
+    .map(i => start[i] + start[i + 1])
+    .forEach(pair => repartition[pair]++)
+
+  return [repartition, rules]
+}
+
+function reduceAsObject(letterCount: Array<[string, number]>) {
+  return letterCount.reduce((acc, [key, value]) => {
+    if (acc[key]) {
+      acc[key] = acc[key] + value
+    } else {
+      acc[key] = value
+    }
+    return acc
+  }, {})
+}
+
+export function stepRepartition(
+  rules: Map<string, string[]>,
+  repartition: Record<string, number>,
+): Record<string, number> {
+  const newPairCount = Object.entries(repartition).flatMap(([pair, count]) => {
+    return rules.get(pair).map(newPair => [newPair, count] as [string, number])
+  })
+  return reduceAsObject(newPairCount)
+}
+
+export function diffMaxMin(repartition: Record<string, number>): number {
+  const letterCount = Object.entries(repartition).flatMap(([pair, count]) => {
+    return [...pair].map(l => [l, count] as [string, number])
+  })
+
+  const res: Record<string, number> = reduceAsObject(letterCount)
+
+  const halfRes = Object.fromEntries(
+    Object.entries(res) //
+      .map(([pair, count]) => [pair, Math.ceil(count / 2)]),
+  )
+  const values = Object.values(halfRes)
+  return Math.max(...values) - Math.min(...values)
+}
+
+// 40 steps : 3ms
+export function countAfterNStepRepartition(input: string, n: number): number {
+  const startTime = performance.now()
+  const [repartition, rules] = parseRepartition(input)
+  const finalRep = range(1, n).reduce(
+    acc => stepRepartition(rules, acc),
+    repartition,
+  )
+  const diff = diffMaxMin(finalRep)
+  const endTime = performance.now()
+  console.log(`Time: ${endTime - startTime} ms`)
+  return diff
 }
